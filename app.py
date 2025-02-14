abea9690e0b143cc240462569b3bf13f30f9744a
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-02-14 19:14:36 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from transformers import pipeline
+import torch
+
+
+#!git config --global user.name "anamargarida"
+#!git config --global user.email "anamargaridasilva320@gmail.com"
+
+# Define the model from Hugging Face Hub
+model_name = "anamargarida/my_model_larger_dataset"
+device = 0 if torch.cuda.is_available() else -1  # Use GPU if available, otherwise CPU
+classifier = pipeline('text-classification', model=model_name, device=device)
+
+# Streamlit interface
+st.title("Text Classification with My Model")
+input_text = st.text_area("Enter a sentence for classification")
+
+if st.button("Classify"):
+    result = classifier(input_text)
+    st.write(result)
