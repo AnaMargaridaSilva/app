b81444f747c1ba7b476a3a9dd0b19dfd48e5e640
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-02-22 19:15:24 +0000

    Update ST2ModelV2.py

--- ST2ModelV2.py
+++ ST2ModelV2.py
@@ -39,7 +39,7 @@ class ST2ModelV2(nn.Module):
         self.config = AutoConfig.from_pretrained(args.model_name)
 
         # Define classifier layers
-        classifier_dropout = 0.3
+        classifier_dropout = self.args.dropout
         self.dropout = nn.Dropout(classifier_dropout)
         self.classifier = nn.Linear(self.config.hidden_size, 6)
 
@@ -81,7 +81,7 @@ class ST2ModelV2(nn.Module):
         sequence_output = self.dropout(sequence_output)
         logits = self.classifier(sequence_output)
 
-        # Split logits for different tasks
+        # Split logits 
         start_arg0_logits, end_arg0_logits, start_arg1_logits, end_arg1_logits, start_sig_logits, end_sig_logits = logits.split(1, dim=-1)
         start_arg0_logits = start_arg0_logits.squeeze(-1)
         end_arg0_logits = end_arg0_logits.squeeze(-1)
@@ -94,29 +94,7 @@ class ST2ModelV2(nn.Module):
         if self.args.signal_classification and not self.args.pretrained_signal_detector:
             signal_classification_logits = self.signal_classifier(sequence_output[:, 0, :])
 
-        # Compute loss if labels are provided
-        loss_fct = nn.CrossEntropyLoss()
-        total_loss = None
-
-        if start_positions is not None and end_positions is not None:
-            start_arg0_loss = loss_fct(start_arg0_logits, start_positions[:, 0])
-            end_arg0_loss = loss_fct(end_arg0_logits, end_positions[:, 0])
-            arg0_loss = (start_arg0_loss + end_arg0_loss) / 2
-
-            start_arg1_loss = loss_fct(start_arg1_logits, start_positions[:, 1])
-            end_arg1_loss = loss_fct(end_arg1_logits, end_positions[:, 1])
-            arg1_loss = (start_arg1_loss + end_arg1_loss) / 2
-
-            start_sig_loss = loss_fct(start_sig_logits, start_positions[:, 2])
-            end_sig_loss = loss_fct(end_sig_logits, end_positions[:, 2])
-            sig_loss = (start_sig_loss + end_sig_loss) / 2
-
-            if self.args.signal_classification and not self.args.pretrained_signal_detector:
-                signal_classification_labels = (end_positions[:, 2] != -100).long()
-                signal_classification_loss = loss_fct(signal_classification_logits, signal_classification_labels)
-                total_loss = (arg0_loss + arg1_loss + sig_loss + signal_classification_loss) / 4
-            else:
-                total_loss = (arg0_loss + arg1_loss + sig_loss) / 3
+        
 
         return {
             'start_arg0_logits': start_arg0_logits,
@@ -125,7 +103,168 @@ class ST2ModelV2(nn.Module):
             'end_arg1_logits': end_arg1_logits,
             'start_sig_logits': start_sig_logits,
             'end_sig_logits': end_sig_logits,
-            'signal_classification_logits': signal_classification_logits,
-            'loss': total_loss,
+            'signal_classification_logits': signal_classification_logits
         }
+
+     def position_selector(
+        self,
+        start_cause_logits, 
+        start_effect_logits, 
+        end_cause_logits, 
+        end_effect_logits,
+        attention_mask,
+        word_ids,
+     ):
+        # basic post processing (removing logits from [CLS], [SEP], [PAD])
+        start_cause_logits -= (1 - attention_mask) * 1e4
+        end_cause_logits -= (1 - attention_mask) * 1e4
+        start_effect_logits -= (1 - attention_mask) * 1e4
+        end_effect_logits -= (1 - attention_mask) * 1e4
+
+        start_cause_logits[0] = -1e4
+        end_cause_logits[0] = -1e4
+        start_effect_logits[0] = -1e4
+        end_effect_logits[0] = -1e4
+
+        start_cause_logits[len(word_ids) - 1] = -1e4
+        end_cause_logits[len(word_ids) - 1] = -1e4
+        start_effect_logits[len(word_ids) - 1] = -1e4
+        end_effect_logits[len(word_ids) - 1] = -1e4
+
+        start_cause_logits = torch.log(torch.softmax(start_cause_logits, dim=-1))
+        end_cause_logits = torch.log(torch.softmax(end_cause_logits, dim=-1))
+        start_effect_logits = torch.log(torch.softmax(start_effect_logits, dim=-1))
+        end_effect_logits = torch.log(torch.softmax(end_effect_logits, dim=-1))
+
+        max_arg0_before_arg1 = None
+        for i in range(len(end_cause_logits)):
+            if attention_mask[i] == 0:
+                break
+            for j in range(i + 1, len(start_effect_logits)):
+                if attention_mask[j] == 0:
+                    break
+
+                if max_arg0_before_arg1 is None:
+                    max_arg0_before_arg1 = ((i, j), end_cause_logits[i] + start_effect_logits[j])
+                else:
+                    if end_cause_logits[i] + start_effect_logits[j] > max_arg0_before_arg1[1]:
+                        max_arg0_before_arg1 = ((i, j), end_cause_logits[i] + start_effect_logits[j])
+        
+        max_arg0_after_arg1 = None
+        for i in range(len(end_effect_logits)):
+            if attention_mask[i] == 0:
+                break
+            for j in range(i + 1, len(start_cause_logits)):
+                if attention_mask[j] == 0:
+                    break
+                if max_arg0_after_arg1 is None:
+                    max_arg0_after_arg1 = ((i, j), start_cause_logits[j] + end_effect_logits[i])
+                else:
+                    if start_cause_logits[j] + end_effect_logits[i] > max_arg0_after_arg1[1]:
+                        max_arg0_after_arg1 = ((i, j), start_cause_logits[j] + end_effect_logits[i])
+
+        if max_arg0_before_arg1[1].item() > max_arg0_after_arg1[1].item():
+            end_cause, start_effect = max_arg0_before_arg1[0]
+            start_cause_logits[end_cause + 1:] = -1e4
+            start_cause = start_cause_logits.argmax().item()
+
+            end_effect_logits[:start_effect] = -1e4
+            end_effect = end_effect_logits.argmax().item()
+        else:
+            end_effect, start_cause = max_arg0_after_arg1[0]
+            end_cause_logits[:start_cause] = -1e4
+            end_cause = end_cause_logits.argmax().item()
+
+            start_effect_logits[end_effect + 1:] = -1e4
+            start_effect = start_effect_logits.argmax().item()
+        
+        return start_cause, end_cause, start_effect, end_effect
+
+
+     def beam_search_position_selector(
+        self,
+        start_cause_logits, 
+        start_effect_logits, 
+        end_cause_logits, 
+        end_effect_logits,
+        attention_mask,
+        word_ids,
+        topk=5
+     ):
+        # basic post processing (removing logits from [CLS], [SEP], [PAD])
+
+        start_cause_logits -= (1 - attention_mask) * 1e4
+        end_cause_logits -= (1 - attention_mask) * 1e4
+        start_effect_logits -= (1 - attention_mask) * 1e4
+        end_effect_logits -= (1 - attention_mask) * 1e4
+
+        start_cause_logits[0] = -1e4
+        end_cause_logits[0] = -1e4
+        start_effect_logits[0] = -1e4
+        end_effect_logits[0] = -1e4
+
+        start_cause_logits[len(word_ids) - 1] = -1e4
+        end_cause_logits[len(word_ids) - 1] = -1e4
+        start_effect_logits[len(word_ids) - 1] = -1e4
+        end_effect_logits[len(word_ids) - 1] = -1e4
+
+        start_cause_logits = torch.log(torch.softmax(start_cause_logits, dim=-1))
+        end_cause_logits = torch.log(torch.softmax(end_cause_logits, dim=-1))
+        start_effect_logits = torch.log(torch.softmax(start_effect_logits, dim=-1))
+        end_effect_logits = torch.log(torch.softmax(end_effect_logits, dim=-1))
+
+        scores = dict()
+        for i in range(len(end_cause_logits)):
+            if attention_mask[i] == 0:
+                break
+            for j in range(i + 1, len(start_effect_logits)):
+                if attention_mask[j] == 0:
+                    break
+                scores[str((i, j, "before"))] = end_cause_logits[i].item() + start_effect_logits[j].item()
+        
+        for i in range(len(end_effect_logits)):
+            if attention_mask[i] == 0:
+                break
+            for j in range(i + 1, len(start_cause_logits)):
+                if attention_mask[j] == 0:
+                    break
+                scores[str((i, j, "after"))] = start_cause_logits[j].item() + end_effect_logits[i].item()
+        
+        
+        topk_scores = dict()
+        for i, (index, score) in enumerate(sorted(scores.items(), key=lambda x: x[1], reverse=True)[:topk]):
+            if eval(index)[2] == 'before':
+                end_cause = eval(index)[0]
+                start_effect = eval(index)[1]
+
+                this_start_cause_logits = start_cause_logits.clone()
+                this_start_cause_logits[end_cause + 1:] = -1e9
+                start_cause_values, start_cause_indices = this_start_cause_logits.topk(topk)
+
+                this_end_effect_logits = end_effect_logits.clone()
+                this_end_effect_logits[:start_effect] = -1e9
+                end_effect_values, end_effect_indices = this_end_effect_logits.topk(topk)
+
+                for m in range(len(start_cause_values)):
+                    for n in range(len(end_effect_values)):
+                        topk_scores[str((start_cause_indices[m].item(), end_cause, start_effect, end_effect_indices[n].item()))] = score + start_cause_values[m].item() + end_effect_values[n].item()
+
+            elif eval(index)[2] == 'after':
+                start_cause = eval(index)[1]
+                end_effect = eval(index)[0]
+
+                this_end_cause_logits = end_cause_logits.clone()
+                this_end_cause_logits[:start_cause] = -1e9
+                end_cause_values, end_cause_indices = this_end_cause_logits.topk(topk)
+
+                this_start_effect_logits = start_effect_logits.clone()
+                this_start_effect_logits[end_effect + 1:] = -1e9
+                start_effect_values, start_effect_indices = this_start_effect_logits.topk(topk)
+
+                for m in range(len(end_cause_values)):
+                    for n in range(len(start_effect_values)):
+                        topk_scores[str((start_cause, end_cause_indices[m].item(), start_effect_indices[n].item(), end_effect))] = score + end_cause_values[m].item() + start_effect_values[n].item()
+
+        first, second = sorted(topk_scores.items(), key=lambda x: x[1], reverse=True)[:2]
+        return eval(first[0]), eval(second[0]), first[1], second[1], topk_scores
 
