ea90e94faf66558eace1d7a770b34b3cd62cd9e9
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-02-22 17:02:46 +0000

    Update Extraction.py

--- Extraction.py
+++ Extraction.py
@@ -11,7 +11,7 @@ login(token=hf_token)
 # Load model & tokenizer once (cached for efficiency)
 @st.cache_resource
 def load_model():
-    model_name = "anamargarida/Extraction"  # Example, update if needed
+    model_name = "anamargarida/Extraction_withseed777" # Example, update if needed
     
     # Load the configuration and tokenizer from Hugging Face Hub
     config = AutoConfig.from_pretrained(model_name)
