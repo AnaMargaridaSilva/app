3d8d39dffa62585670189b9cf46bbd9f7481edd5
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-02-22 20:36:30 +0000

    Update Extraction4.py

--- Extraction4.py
+++ Extraction4.py
@@ -94,7 +94,7 @@ def extract_arguments(text, tokenizer, model, beam_search=True):
         end_signal_logits[start_signal1 + 5:] = -1e4
         end_signal1 = end_signal_logits.argmax().item()
 
-     tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
+    tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
 
     def extract_span(start, end):
         return tokenizer.convert_tokens_to_string(tokens[start:end+1]) if start is not None and end is not None else ""
