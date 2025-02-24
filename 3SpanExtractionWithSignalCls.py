a4726cc431269c3ca2849a23849e4586d6e6ed76
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-02-24 14:13:22 +0000

    Update 3SpanExtractionWithSignalCls.py

--- 3SpanExtractionWithSignalCls.py
+++ 3SpanExtractionWithSignalCls.py
@@ -138,7 +138,7 @@ def extract_arguments(text, tokenizer, model, beam_search=True):
     #for position, (token_id, token) in enumerate(zip(token_ids, tokens)):
         #st.write(f"Position: {position}, ID: {token_id}, Token: {token}")
 
-    st.write(f"Start Cause 1: {start_cause1}, End Cause: {end_causet2}")
+    st.write(f"Start Cause 1: {start_cause1}, End Cause: {end_cause2}")
     st.write(f"Start Effect 1: {start_effect1}, End Cause: {end_effect2}")
     st.write(f"Start Signal: {start_signal}, End Signal: {end_signal}")
 
